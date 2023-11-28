800d00bfa02e4497f31333dd9271d3753cf92d78
Diya Mahaveer <diyamahaveer@gmail.com>
2023-11-28 17:04:39 -0500

    testing bor

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import './LoginSignup.css'
 
 const LoginSignup = () => {
   return (
-    <div>
-      
+    <div className = 'loginsignup'>
+      <div className = 'loginsignup-container'>
+        <h1>Sign Up</h1>
+        <h3>Start your shopping journey with ClosetSwap!</h3>
+        <div className="loginsignup-fields">
+          <input type = "text" placeholder = 'Name' />
+          <input type = "email" placeholder = 'Email Address' />
+          <input type = "password" placeholder = 'Password' />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
+        <div className = "loginsignup-agree">
+          <input type = "checkbox" name= '' id= '' />
+          <p>By continuing, I agree to the terms of use & privacy policy</p>
+        </div> 
+      </div>
     </div>
   )
 }
